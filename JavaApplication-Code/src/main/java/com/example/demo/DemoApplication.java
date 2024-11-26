6ddf0ac1d9702fefc0c64380cb3390fbae08b196
Michaelgwei86 <michaelgwei86@gmail.com>
2024-11-26 16:40:50 +0100

    updated code base

--- JavaApplication-Code/src/main/java/com/example/demo/DemoApplication.java
+++ JavaApplication-Code/src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		String DBPassword = "Admin@1234";
+	//	String DBPassword = "Admin@1234";
 	}
 
 }
